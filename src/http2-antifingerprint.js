da0274d682486ca7579dbcea349b5e2670f9edd2
thepicture <85333076+thepicture@users.noreply.github.com>
2022-12-05 16:27:45 -0500

    feat(http2-antifingerprint): add proxy support

--- src/http2-antifingerprint.js
+++ src/http2-antifingerprint.js
@@ -1,4 +1,7 @@
+const http = require("node:http");
 const http2 = require("node:http2");
+
+const tls = require("node:tls");
 
 const {
   AntiFingerprintClientSessionOptions,
@@ -6,12 +9,77 @@ const {
 
 const { randint } = require("./randint.js");
 
-function connect(authority, listener) {
-  const client = http2.connect(
-    authority,
-    new AntiFingerprintClientSessionOptions().get(),
-    listener
-  );
+async function connect(authority, listener, options) {
+  let isAuthenticatedProxy = false;
+  let proxy = "";
+  let optionsProxy;
+  let onSwitchingProtocols = () => {};
+
+  if (typeof options === "object") {
+    optionsProxy = options.proxy;
+
+    if (optionsProxy) {
+      if (optionsProxy.user || optionsProxy.password) {
+        isAuthenticatedProxy = true;
+        proxy = `${optionsProxy.scheme}://${optionsProxy.user}:${optionsProxy.password}@${optionsProxy.host}:${optionsProxy.port}`;
+      } else {
+        proxy = `${optionsProxy.scheme}://${optionsProxy.host}:${optionsProxy.port}`;
+      }
+    }
+
+    const optionsOnSwitchingProtocols = options.onSwitchingProtocols;
+
+    if (optionsOnSwitchingProtocols) {
+      onSwitchingProtocols = optionsOnSwitchingProtocols;
+    }
+  }
+
+  let client;
+
+  const port = authority.startsWith("http://") ? 80 : 443;
+
+  const host = authority.includes("http://")
+    ? authority.split("http://")[1]
+    : authority.split("https://")[1];
+
+  if (proxy) {
+    const headers = {};
+    if (isAuthenticatedProxy) {
+      headers["Proxy-Authorization"] = `Basic ${Buffer.from(
+        optionsProxy.user + ":" + optionsProxy.password
+      ).toString("base64")}`;
+    }
+    const request = http.request({
+      method: "CONNECT",
+      host: optionsProxy.host,
+      port: optionsProxy.port,
+      path: `${host}:${port}`,
+      headers,
+    });
+    request.end();
+
+    client = await new Promise((resolve) => {
+      request.on("connect", (response, socket) => {
+        onSwitchingProtocols(response);
+        resolve(
+          http2.connect(authority, {
+            createConnection: () =>
+              tls.connect({
+                host,
+                socket: socket,
+                ALPNProtocols: ["h2"],
+              }),
+          })
+        );
+      });
+    });
+  } else {
+    client = http2.connect(
+      authority,
+      new AntiFingerprintClientSessionOptions().get(),
+      listener
+    );
+  }
 
   const originalRequest = client.request;
 
